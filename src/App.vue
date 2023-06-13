e12cfc4e8c31f962f215ec5f378752b32259393b
silence870121 <silence870121@gmail.com>
2023-06-13 10:45:54 +0800

    Create Theme component

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,77 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import Router from './components/Router.vue'
+import Theme from './components/Theme.vue'
 </script>
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> | <router-link to="/about">About</router-link> |
-    <router-link to="/contact">Contact</router-link>
-  </nav>
-  <main>
-    <router-view />
-  </main>
+  <Theme />
+  <div class="theme-display">
+    <div class="primary">primary</div>
+    <div class="secondary">secondary</div>
+    <div class="background">background</div>
+    <div class="surface">surface</div>
+  </div>
+  <Router />
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Atomission logo" />
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue + " />
 </template>
 
-
 <style scoped>
+:root {
+  --primary-color: white;
+  --secondary-color: purple;
+}
+
+.theme-btn {
+  background-color: var(--primary-color);
+  color: var(--on-primary-color);
+  transition: all 200ms linear;
+}
+
+.theme-display {
+  padding: 1rem;
+  margin: 1rem;
+  background-color: rgb(128, 128, 128);
+  display: flex;
+  border: 1px solid var(--grid-color);
+  border-radius: 0.5rem;
+}
+
+.theme-display>div {
+  padding: 0.5rem 1rem;
+  border: 1px solid var(--grid-color);
+  margin: 0.5rem;
+  border-radius: 0.5rem;
+  transition: all 200ms linear;
+}
+
+.primary {
+  background-color: var(--primary-color);
+  color: var(--on-primary-color);
+}
+
+.secondary {
+  background-color: var(--secondary-color);
+  color: var(--on-secondary-color);
+}
+
+.background {
+  background-color: var(--background-color);
+  color: var(--on-background-color);
+}
+
+.surface {
+  background-color: var(--surface-color);
+  color: var(--on-surface-color);
+}
+
 .logo {
   height: 6em;
   padding: 1.5em;
---
+++ src/components/Theme.vue
@@ -0,0 +1,73 @@
+<script setup>
+//? Default Theme Style
+let THEME_STYLE = false
+
+//? Theme Style Settings
+const LIGHT_THEME = new Map([
+    ['--primary-color', 'blue'],
+    ['--secondary-color', 'purple'],
+    ['--background-color', 'white'],
+    ['--surface-color', 'gray'],
+
+    ['--on-primary-color', 'white'],
+    ['--on-secondary-color', 'white'],
+    ['--on-background-color', 'black'],
+    ['--on-surface-color', 'black'],
+    []
+])
+const DARK_THEME = new Map([
+    ['--primary-color', 'gold'],
+    ['--secondary-color', 'purple'],
+    ['--background-color', 'black'],
+    ['--surface-color', 'gray'],
+
+    ['--on-primary-color', 'black'],
+    ['--on-secondary-color', 'black'],
+    ['--on-background-color', 'white'],
+    ['--on-surface-color', 'white'],
+    []
+])
+
+//? Set style root 
+const setProperty = (config) => {
+    if (!(config instanceof Map)) {
+        throw new Error('config is not a Map');
+    }
+    config.forEach((value, key) => {
+        document.documentElement.style.setProperty(key, value);
+    })
+}
+//? Default Theme Style
+const setDefaultProperty = setProperty(LIGHT_THEME)
+
+//? Switch Theme Style
+function switchTheme() {
+    switch (THEME_STYLE) {
+        case true:
+            setProperty(LIGHT_THEME)
+            document.querySelector('.theme-btn').innerHTML = "Light Mode";
+            break;
+        case false:
+            setProperty(DARK_THEME)
+            document.querySelector('.theme-btn').innerHTML = "Dark Mode"
+            break;
+
+        default:
+            break;
+    }
+    THEME_STYLE = !THEME_STYLE
+}
+</script>
+
+<template>
+    <button class="theme-btn" @click="switchTheme">Light Mode</button>
+</template>
+
+<style>
+:root {
+    --primary-color: white;
+    --secondary-color: purple;
+    --on-primary-color: black;
+    --grid-color: rgb(128, 128, 128);
+}
+</style>
