43a4946fafba223b8f6568aebcc4a8d129fdaf33
silence870121 <silence870121@gmail.com>
2023-07-05 11:24:44 +0800

    Update Main style

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,31 @@
 import Navigation from './components/Router.vue'
 </script>
 <template>
-  <Navigation />
-  <main>
-    <router-view />
-    <div>
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo" alt="Vite logo" />
-      </a>
-    </div>
-  </main>
+	<Navigation />
+	<main>
+		<router-view />
+		<div>
+			<a href="https://vitejs.dev" target="_blank">
+				<img src="/vite.svg" class="logo" alt="Vite logo" />
+			</a>
+		</div>
+	</main>
 </template>
 
 <style scoped>
 main {
-  height: 100%;
-  flex: 1;
-  padding: 2rem;
-  display: flex;
-  justify-content: start;
-  align-items: center;
-  flex-direction: column;
+	height: 100%;
+	flex: 1;
+	padding: 2rem;
+	display: flex;
+	justify-content: start;
+	align-items: center;
+	flex-direction: column;
+}
+
+@media (max-width: 375px) {
+	main {
+		padding: 2rem 15px;
+	}
 }
 </style>
