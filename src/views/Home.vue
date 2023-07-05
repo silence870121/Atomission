7d63d0c436ab25e640bde7ca64e44faf92b835e2
silence870121 <silence870121@gmail.com>
2023-07-05 11:25:50 +0800

    add target component in Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,33 @@
+<script setup>
+import TargetCard from '@/components/Project/TargetCard.vue';
+</script>
+
 <template>
-    <h2> Home </h2>
+    <div class="target-simplify">
+        <TargetCard :title="'First Target'" :progress=100 />
+        <TargetCard :title="'First Target'" :progress=100 />
+        <TargetCard :title="'First Target'" :progress=100 />
+        <TargetCard :title="'First Target'" :progress=100 />
+    </div>
 </template>
+
+<style scoped>
+.target-simplify {
+    width: 100%;
+    display: flex;
+    gap: 0.5rem;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.target-simplify>>>.target-svg {
+    width: 3rem;
+    height: 3rem;
+}
+
+.target-simplify .target-card {
+    flex: 1;
+}
+</style>
