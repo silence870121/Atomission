1bf7f6d3fbec05c5185ab5ab882dc7c0e80ed54f
Herbert (WEI-CHE, Chung) <silence870121@gmail.com>
2024-04-20 07:58:19 +0800

    refactor: Move Header Component to global from views component.
    在全域註冊 Header 組件，並將 View 組件中的 Header 組件移除。
    
    issue #59

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import 'virtual:svg-icons-register'
 import SvgIcon from './components/SvgIcon.vue'
 import ThemeBtn from './components/Theme.vue'
 import Router from './components/Router.vue'
+import Header from '@/components/Header.vue';
 import { createPinia } from 'pinia'
 
 const pinia = createPinia()
@@ -13,6 +14,7 @@ const APP = createApp(App)
 APP.component('svg-icon', SvgIcon)
 APP.component('ThemeBtn', ThemeBtn)
 APP.component('Router', Router)
+APP.component('Header', Header)
 APP.use(router)
 APP.use(pinia)
 APP.mount('#app')
