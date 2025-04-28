e22f7d90e7e8596916d99550a614685cc3948f30
Herbert (WEI-CHE, Chung) <silence870121@gmail.com>
2025-04-28 15:33:59 +0800

    fix: Use if condition to prevent a crash when no data is provided.
    當未提供資料時，程式仍嘗試存取指定的物件屬性，導致後續代碼執行錯誤。
    因此添加 if 判斷，確保在資料未傳入時直接終止執行，防止異常發生。

--- src/components/Masonry.vue
+++ src/components/Masonry.vue
@@ -53,7 +53,6 @@ function renderMasonry() {
     columnsCount.value = Math.min(Math.max(Math.floor(masonryWidth.value / (props.columnWidth + props.columnGap)), 1), props.maxColumn)
     resetMasonryColumn()
     appendCell(props.data)
-
 }
 window.onresize = () => { renderMasonry() }
 
@@ -66,15 +65,17 @@ function resetMasonryColumn() {
     }
 }
 function appendCell(data) {
+    if (!data) return
+    //? 判斷是否有資料
     data.forEach(item => {
-        if (!item.list) return
+        if (!item.task) return
         //? 判斷最低高度
         const minHeight = Math.min(...columnHeights.value)
         //? 查詢最低高度的陣列索引
         const indexOfMinHeight = columnHeights.value.indexOf(minHeight)
 
         //? 對 最低高度的數組 增加 當前物件的高度
-        columnHeights.value[indexOfMinHeight] += item.list.length + 320
+        columnHeights.value[indexOfMinHeight] += item.task.length + 320
 
         //? 在 最低高度的次級列表 加入 當前的物件
         columnList.value[indexOfMinHeight].push(item)
@@ -92,10 +93,16 @@ onMounted(() => {
 </script>
 
 <template>
-    <div class="warning" v-if="hasData"> No any mission</div>
+    <div class="masonry-warning">
+        <p v-if="props.data.length == 0"> No any mission</p>
+        <p v-else-if="hasData">
+            Your mission data format is incorrect.<br />
+            Please refresh this page.
+        </p>
+    </div>
     <div class="masonry-row" :style="{ '--gap': props.rowGap }">
-        <ul v-for="list in columnList" class="masonry-column" :style="{ '--gap': props.columnGap }">
-            <li v-for="item in list" class="masonry-cell">
+        <ul v-for="column in columnList" class="masonry-column" :style="{ '--gap': props.columnGap }">
+            <li v-for="item in column" class="masonry-cell">
                 <MissionCard :mission="item" />
             </li>
         </ul>
@@ -122,5 +129,15 @@ onMounted(() => {
 .masonry-cell {
     display: flex;
     justify-content: center;
+}
+
+.masonry-warning {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 2rem;
+    text-transform: capitalize;
+    text-align: center;
+    line-height: 1.5rem;
 }
 </style>
