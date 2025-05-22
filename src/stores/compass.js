5bd669b78023c719b0489bf3956db47363e7f093
Herbert (WEI-CHE, Chung) <silence870121@gmail.com>
2025-05-22 13:55:59 +0800

    feat: Create JSON to store constant data, and remove constant data from other files
    建立常數用的JSON文件，並將各個檔案中的常數數據整理至文件中。
    以利統一管理常數數據，避免不同檔案重複定義。

--- src/stores/compass.js
+++ src/stores/compass.js
@@ -1,43 +1,22 @@
 import { defineStore } from "pinia";
 import { useDataStore } from "@stores/data"
 import { ref } from "vue";
+import defaultData from "@/assets/default.json";
+
 
 export const useCompassStore = defineStore('compass', () => {
     const compassDataStore = useDataStore().db_compass
     //? 初始化狀態
     let isCardMode = ref(true)
-    const DEFAULT_COMPASS = {
-        id: "compassID",
-        name: "compassName",
-        stage: "compassStage",
-        landmark: 1000,
-        score: 999,
-        aptness: "",
-        create_date: "yyyy-mm-dd",
-        edit_date: "",
-        isActive: true,
-        halveScore: false,
-        isMarked: false,
-        children: [],
-    }
-    let compassDataList = [{
-        id: "c0001",
-        name: "My First Compass",
-        stage: "beginning",
-        landmark: 1000,
-        score: 999,
-        aptness: "",
-        create_date: "2024-01-01",
-        edit_date: "",
-        isActive: true,
-        halveScore: false,
-        isMarked: false,
-        children: []
-    }]
+    const DEFAULT_COMPASS = defaultData.compass
 
+    //? Mission's edit database
+    let compassEditor = ref({ ...defaultData.compass })
+    let compassList = ref([])
     return {
         isCardMode,
         DEFAULT_COMPASS,
-        compassDataList,
+        compassEditor,
+        compassList,
     }
 })
--- src/stores/mission.js
+++ src/stores/mission.js
@@ -7,24 +7,7 @@ export const useMissionStore = defineStore('mission', () => {
 
     //? CONSTANTS
     const DEFAULT_MISSION = defaultData.mission
-    const OPTION = {
-        type: {
-            daily: '每日',
-            weekly: '每週',
-            dungeon: '副本',
-            series: '系列',
-            limit: '限時',
-            once: '單次',
-            loop: '循環'
-        },
-        level: {
-            easy: '簡單',
-            normal: '普通',
-            hard: '困難',
-            expert: '專家',
-            master: '大師'
-        }
-    }
+
     //? Mission's edit database
     let missionEditor = ref({ ...defaultData.mission })
     let missionList = ref([])
@@ -43,7 +26,6 @@ export const useMissionStore = defineStore('mission', () => {
     return {
         sortTask,
         DEFAULT_MISSION,
-        OPTION,
         missionEditor,
         missionList,
         getAllMission
